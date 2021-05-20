d6466d3414d834222f96c615cf0ad8b0e1b19d4a
Christoffer Holmström <a20chrho@student.his.se>
2021-05-20 12:40:42 +0200

    - added a second activity and button access to both activities

--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -5,16 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+public class MainActivity2 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
-
     /** Called when the user taps the Act1Button */
     public void GoToAct1(View view) {
         Intent intent = new Intent(this, MainActivity.class);
@@ -26,5 +24,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
     }
-
 }
